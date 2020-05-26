014d92dd8a60022f20f2d0654b685f884a2bfc60
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2020-05-26 13:46:50 +0200

    added test for workflow and sub-workflow

---
+++ afclAPI/src/test/java/afcl/SubFCTest.java
@@ -0,0 +1,115 @@
+package afcl;
+
+import afcl.functions.AtomicFunction;
+import afcl.functions.Compound;
+import afcl.functions.objects.DataIns;
+import afcl.functions.objects.DataOuts;
+import afcl.functions.objects.PropertyConstraint;
+import org.junit.Assert;
+import org.junit.Test;
+import org.meanbean.test.BeanTester;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SubFCTest {
+    /**
+     * Test full construction of a subFC.
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testFullConstruction() {
+        AtomicFunction atomicFunction = new AtomicFunction("atomicFunction", "atomicFunctionType", null, null);
+        DataIns dataIns = new DataIns("inName", "inType");
+        DataOuts dataOuts = new DataOuts("outName", "outType", "outSource");
+
+        SubFC subFC = new SubFC("subFC",
+                new ArrayList<>(Collections.singleton(dataIns)),
+                new ArrayList<>(Collections.singleton(atomicFunction)),
+                new ArrayList<>(Collections.singleton(dataOuts)));
+
+        Assert.assertEquals("subFC", subFC.getName());
+
+        Assert.assertEquals(1, subFC.getDataIns().size());
+        Assert.assertEquals(dataIns, subFC.getDataIns().get(0));
+        Assert.assertEquals(dataIns.hashCode(), subFC.getDataIns().get(0).hashCode());
+
+        Assert.assertEquals(1, subFC.getSubFCBody().size());
+        Assert.assertEquals(atomicFunction, subFC.getSubFCBody().get(0));
+        Assert.assertEquals(atomicFunction.hashCode(), subFC.getSubFCBody().get(0).hashCode());
+
+        Assert.assertEquals(1, subFC.getDataOuts().size());
+        Assert.assertEquals(dataOuts, subFC.getDataOuts().get(0));
+        Assert.assertEquals(dataOuts.hashCode(), subFC.getDataOuts().get(0).hashCode());
+
+        Assert.assertEquals(0, atomicFunction.getAdditionalProperties().size());
+    }
+
+    /**
+     * Test the empty construction of a subFC.
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testEmptyConstruction() {
+        SubFC subFC = new SubFC();
+
+        Assert.assertNull(subFC.getName());
+        Assert.assertNull(subFC.getDataIns());
+        Assert.assertNull(subFC.getSubFCBody());
+        Assert.assertNull(subFC.getDataOuts());
+    }
+
+    /**
+     * Test getter and setter
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testGetterAndSetter() {
+        new BeanTester().testBean(SubFC.class);
+    }
+
+    /**
+     * Test hashCode and equals
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testHashEquals() {
+        SubFC subFC = new SubFC("name", null, null, null);
+
+        Assert.assertEquals(subFC, subFC);
+        Assert.assertEquals(subFC.hashCode(), subFC.hashCode());
+        Assert.assertNotEquals(subFC, null);
+
+        Compound compound = new Compound();
+        Assert.assertNotEquals(subFC, compound);
+
+        SubFC subFC2 = new SubFC("name", null, null, null);
+        Assert.assertEquals(subFC, subFC2);
+        Assert.assertEquals(subFC.hashCode(), subFC2.hashCode());
+
+        SubFC subFC3;
+        subFC3 = new SubFC("nameWrong", null, null, null);
+        Assert.assertNotEquals(subFC, subFC3);
+
+        subFC3 = new SubFC("name", null, null, null);
+        subFC3.setConstraints(Collections.singletonList(new PropertyConstraint()));
+        Assert.assertNotEquals(subFC, subFC3);
+
+        subFC3 = new SubFC("name", null, null, null);
+        subFC3.setProperties(Collections.singletonList(new PropertyConstraint()));
+        Assert.assertNotEquals(subFC, subFC3);
+
+        subFC3 = new SubFC("name", Collections.singletonList(new DataIns("name", "type", "source")), null, null);
+        Assert.assertNotEquals(subFC, subFC3);
+
+        subFC3 = new SubFC("name", null, Collections.singletonList(new AtomicFunction()), null);
+        Assert.assertNotEquals(subFC, subFC3);
+
+        subFC3 = new SubFC("name", null, null, Collections.singletonList(new DataOuts("name", "type", "source")));
+        Assert.assertNotEquals(subFC, subFC3);
+    }
+}
---
+++ afclAPI/src/test/java/afcl/WorkflowTest.java
@@ -0,0 +1,110 @@
+package afcl;
+
+import afcl.functions.AtomicFunction;
+import afcl.functions.Compound;
+import afcl.functions.objects.DataIns;
+import afcl.functions.objects.DataOuts;
+import org.junit.Assert;
+import org.junit.Test;
+import org.meanbean.test.BeanTester;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class WorkflowTest {
+    /**
+     * Test full construction of a workflow.
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testFullConstruction() {
+        AtomicFunction atomicFunction = new AtomicFunction("atomicFunction", "atomicFunctionType", null, null);
+        DataIns dataIns = new DataIns("inName", "inType");
+        DataOuts dataOuts = new DataOuts("outName", "outType", "outSource");
+
+        Workflow workflow = new Workflow("workflow",
+                new ArrayList<>(Collections.singleton(dataIns)),
+                new ArrayList<>(Collections.singleton(atomicFunction)),
+                new ArrayList<>(Collections.singleton(dataOuts)));
+
+        Assert.assertEquals("workflow", workflow.getName());
+
+        Assert.assertEquals(1, workflow.getDataIns().size());
+        Assert.assertEquals(dataIns, workflow.getDataIns().get(0));
+        Assert.assertEquals(dataIns.hashCode(), workflow.getDataIns().get(0).hashCode());
+
+        Assert.assertEquals(1, workflow.getWorkflowBody().size());
+        Assert.assertEquals(atomicFunction, workflow.getWorkflowBody().get(0));
+        Assert.assertEquals(atomicFunction.hashCode(), workflow.getWorkflowBody().get(0).hashCode());
+
+        Assert.assertEquals(1, workflow.getDataOuts().size());
+        Assert.assertEquals(dataOuts, workflow.getDataOuts().get(0));
+        Assert.assertEquals(dataOuts.hashCode(), workflow.getDataOuts().get(0).hashCode());
+
+        Assert.assertEquals(0, atomicFunction.getAdditionalProperties().size());
+    }
+
+    /**
+     * Test the empty construction of a workflow.
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testEmptyConstruction() {
+        Workflow workflow = new Workflow();
+
+        Assert.assertNull(workflow.getName());
+        Assert.assertNull(workflow.getDataIns());
+        Assert.assertNull(workflow.getWorkflowBody());
+        Assert.assertNull(workflow.getDataOuts());
+    }
+
+    /**
+     * Test getter and setter
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testGetterAndSetter() {
+        new BeanTester().testBean(Workflow.class);
+    }
+
+    /**
+     * Test hashCode and equals
+     *
+     * @author stefanpedratscher
+     */
+    @Test
+    public void testHashEquals() {
+        Workflow workflow = new Workflow("name", null, null, null);
+
+        Assert.assertEquals(workflow, workflow);
+        Assert.assertEquals(workflow.hashCode(), workflow.hashCode());
+        Assert.assertNotEquals(workflow, null);
+
+        Compound compound = new Compound();
+        Assert.assertNotEquals(workflow, compound);
+
+        Workflow workflow2 = new Workflow("name", null, null, null);
+        Assert.assertEquals(workflow, workflow2);
+        Assert.assertEquals(workflow.hashCode(), workflow2.hashCode());
+
+        Workflow workflow3;
+        workflow3 = new Workflow("nameWrong", null, null, null);
+        Assert.assertNotEquals(workflow, workflow3);
+
+        workflow3 = new Workflow("name", null, null, null);
+        workflow3.setSubFCs(Collections.singletonList(new SubFC()));
+        Assert.assertNotEquals(workflow, workflow3);
+
+        workflow3 = new Workflow("name", Collections.singletonList(new DataIns("name", "type", "source")), null, null);
+        Assert.assertNotEquals(workflow, workflow3);
+
+        workflow3 = new Workflow("name", null, Collections.singletonList(new AtomicFunction()), null);
+        Assert.assertNotEquals(workflow, workflow3);
+
+        workflow3 = new Workflow("name", null, null, Collections.singletonList(new DataOuts("name", "type", "source")));
+        Assert.assertNotEquals(workflow, workflow3);
+    }
+}
